c2a993bf17515dfaace9ab3aa55bcbf6893f7266
Andreas Christensen <andreaschristensen2010@live.dk>
2022-05-06 11:36:14 +0200

    working ex 1 and 2

--- project/main.cpp
+++ project/main.cpp
@@ -1,41 +1,23 @@
 #include <iostream>
+#include <fstream>
 
 #include "json.hpp"
 #include "ticker.hpp"
 
-#include <ctime>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
+const std::string FILE_TO_OPEN {"../testfile.json"};
 
 int main() {
-    //auto dateStr = std::string{"2012-10-31T09:01:00.000+0100"};
-    //auto time = getEpochTime(dateStr);
-    //std::cout << time.tm_hour << std::endl;
-    //std::cout << std::asctime(std::gmtime(&time)) << std::endl;
-
-    auto is = std::istringstream{R"({"tag":"CARL A",
-"isin":"DK0010181676",
-"shares":33699252,
-"name":"Carlsberg Breweries A/S",
-"url":"https://www.carlsberggroup.com/",
-"market":"Nasdaq Copenhagen",
-"sector":"Food, Beverage and Tobacco",
-"segment":"Large",
-"trades":[
-{"time":"2012-10-31T09:01:00.000+0100",
-"price":527,
-"amount":35,
-"buyer":"DDB",
-"seller":"NDA",
-"seq":1,
-"code":0}
-]})"};
-    auto t = ticker{};
-    is >> json_t{t};
-    for (const auto trade : t.trades){
-        std::cout << trade.time.tm_hour << std::endl;
+    // Exercise 1 & 2)
+    std::ifstream file {FILE_TO_OPEN};
+    if(file){
+        auto t = ticker{};
+        file >> json_t{t};
+        std::cout << t.name << std::endl;
     }
+    else{
+        std::cout << "No file found ..." << std::endl;
+    }
+
     return 0;
 }
 
